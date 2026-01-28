01d500048a5e3a3f83420011567f670c82e4cc29
Copca92 <elihucop@gmail.com>
2026-01-28 13:22:16 +0100

    routers

---
+++ app/main.py
@@ -0,0 +1,39 @@
+import zoneinfo
+from datetime import datetime
+
+from fastapi import FastAPI
+from models import Transaction, Invoice
+from db import  SessionDep, create_all_tables
+from sqlmodel import select
+from .routers import customers, transactions, invoices
+
+app = FastAPI(lifespan=create_all_tables)
+app.include_router(customers.router)
+app.include_router(transactions.router)
+app.include_router(invoices.router)
+
+@app.get("/")
+async def root():
+    return {"message": "Hello, Luis!"}
+
+country_timezones = {
+    "CO": "America/Bogota",
+    "US": "America/New_York",
+    "AR": "America/Argentina/Buenos_Aires",
+    "BR": "America/Sao_Paulo",
+    "MX": "America/Mexico_City",
+    "PE": "America/Lima",
+    "CL": "America/Santiago",
+    "VE": "America/Caracas",
+}
+
+
+@app.get("/time/{iso_code}")
+async def time(iso_code: str):
+    iso = iso_code.upper()
+    timezone_str = country_timezones.get(iso)
+    if not timezone_str:
+        return {"error": "Invalid ISO code"}
+    tz = zoneinfo.ZoneInfo(timezone_str)
+    return {"time": datetime.now(tz)}
+   
---
+++ app/routers/customers.py
@@ -0,0 +1,51 @@
+
+from fastapi import APIRouter, HTTPException, status
+from sqlmodel import select
+
+from models import Customer, CustomerCreate, CustomerUpdate
+from db import SessionDep
+
+router = APIRouter()
+
+@router.post("/customers/", response_model=Customer, tags=["customers"])
+async def create_customer(customer_data: CustomerCreate, session: SessionDep):
+    customer = Customer.model_validate(customer_data.model_dump())
+    session.add(customer)
+    session.commit()
+    session.refresh(customer)
+    return customer
+
+@router.get("/customers/{customer_id}", response_model=Customer, tags=["customers"])
+async def read_customer(customer_id: int, session: SessionDep):
+    customer_db = session.get(Customer, customer_id)
+    if not customer_db:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Customer doesnt exist")
+    return customer_db
+
+@router.patch("/customers/{customer_id}", response_model=Customer, status_code=status.HTTP_201_CREATED, tags=["customers"])
+async def update_customer(customer_id: int, customer_data: CustomerUpdate, session: SessionDep):
+    customer_db = session.get(Customer, customer_id)
+    if not customer_db:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Customer doesnt exist")
+    
+    customer_data_dic = customer_data.model_dump(exclude_unset=True)
+    customer_db.sqlmodel_update(customer_data_dic)
+    session.add(customer_db)
+    session.commit()
+    session.refresh(customer_db)
+    return {"detail": "Customer updated successfully"}
+
+
+@router.delete("/customers/{customer_id}", response_model=Customer, tags=["customers"])
+async def delete_customer(customer_id: int, session: SessionDep):
+    customer_db = session.get(Customer, customer_id)
+    if not customer_db:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Customer doesnt exist")
+    session.delete(customer_db)
+    session.commit()
+    return {"detail": "Customer deleted successfully"}
+
+
+@router.get("/customers/", response_model=list[Customer], tags=["customers"])
+async def list_customers(session: SessionDep):
+    return session.exec(select(Customer)).all()
